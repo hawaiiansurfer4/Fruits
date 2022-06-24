a6200260d43d940d39627187cf863b6a63421e0c
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-24 14:46:22 -0400

    feat: add app assets

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Sean Murphy on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
