3cedb6bc9214642d8fe8ce781252b9c45d3020f4
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-28 12:51:00 -0400

    feat: add model and data to display fruits

---
+++ Fruits/Model/FruitModel.swift
@@ -0,0 +1,18 @@
+//
+//  FruitModel.swift
+//  Fruits
+//
+//  Created by Sean Murphy on 6/28/22.
+//
+
+import SwiftUI
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
--- Fruits/Screen/OnboardingView.swift
+++ Fruits/Screen/OnboardingView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct OnboardingView: View {
     
-     
+    var fruits: [Fruit] = fruitsData
     
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                FruitCardView()
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
             }
         }
         .tabViewStyle(PageTabViewStyle())
@@ -24,6 +24,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
