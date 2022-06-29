3c43faf1f80f9ed1607754e887bb84768cc11c28
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-29 15:00:40 -0400

    feat: add settings view

---
+++ Fruits/Screen/SettingsView.swift
@@ -0,0 +1,109 @@
+//
+//  SettingsView.swift
+//  Fruits
+//
+//  Created by Sean Murphy on 6/29/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fruits", labelImage: "info.circle")
+                    ) {
+                        
+                        Divider()
+                            .padding(.vertical, 4)
+                        
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            
+                            Text("Most friuts are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                            
+                        }
+                    }
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider()
+                            .padding(.vertical, 4)
+                        
+                        Text("If you want, you can restart the application by toggling the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                            
+                        )
+                    }
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                        
+                        SettingsRowView(name: "Developer", content: "Sean")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "Github", linkDestination: "https://github.com/hawaiiansurfer4")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                    }
+                    
+                    
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                        }
+                )
+                .padding()
+            }
+            
+            
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ Fruits/View/SettingsLabelView.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsLabelView.swift
+//  Fruits
+//
+//  Created by Sean Murphy on 6/29/22.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    var labelText: String
+    var labelImage: String
+    
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased())
+                .fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fruits", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
